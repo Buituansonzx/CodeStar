025d66d1c54a9e0dd6fb12bc806263e4fff34f96
Tuan Son <bson55444@gmail.com>
2023-12-12 22:22:48 +0700

    homework day16

---
+++ src/main/java/org/example/testsuite/HW_Day16_Test.java
@@ -0,0 +1,22 @@
+package org.example.testsuite;
+
+import org.example.common.CommonBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HW_Day16_Test extends CommonBase {
+    @BeforeMethod
+    public void openChrome(){
+        driver = initChromeDriver("http://seleniumpractise.blogspot.com/2019/01/alert-demo.html");
+    }
+    @Test
+    public void checkAlert(){
+        driver.findElement(By.xpath("//button[@onclick=\"myFunction()\"]")).click();
+        pause(10000);
+        String msg = driver.switchTo().alert().getText();
+        Assert.assertEquals(msg, "Welcome to Selenium WebDriver Tutorials");
+        driver.switchTo().alert().accept();
+    }
+}
